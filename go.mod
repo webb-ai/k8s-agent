d5bf8b8789d377d05b0ec9b38eca5495a831ba2c
Chi Su <chi@webb.ai>
2023-03-28 17:54:26 -0700

    add http library code to do oauth and send messages

--- go.mod
+++ go.mod
@@ -42,6 +42,8 @@ require (
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
---
+++ pkg/http/oauth.go
@@ -0,0 +1,54 @@
+package http
+
+import (
+	"github.com/hashicorp/go-retryablehttp"
+	"io"
+	"k8s.io/apimachinery/pkg/util/json"
+	"net/http"
+)
+
+type tokenJSON struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    int32  `json:"expires_in"`
+}
+
+func GetAccessToken(retryClient *retryablehttp.Client, tokenUrl, clientId, clientSecret, audience string) (string, error) {
+
+	requestBody := map[string]string{
+		"client_id":     clientId,
+		"client_secret": clientSecret,
+		"grant_type":    "client_credentials",
+		"audience":      audience,
+	}
+	requestString, _ := json.Marshal(requestBody)
+
+	response, err := retryClient.Post(tokenUrl, "application/json", requestString)
+	if err != nil {
+		return "", err
+	}
+
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var token tokenJSON
+	err = json.Unmarshal(responseBody, &token)
+	if err != nil {
+		return "", err
+	}
+	return token.AccessToken, nil
+}
+
+func SendRequestWithToken(retryClient *retryablehttp.Client, url, token, body string) (*http.Response, error) {
+	request, err := retryablehttp.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("Authorization", "Bearer "+token)
+	request.Header.Set("Content-Type", "application/json")
+	return retryClient.Do(request)
+}
